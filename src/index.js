c2eacc770585606bbc9e1ac3867452ece328b01e
Shahmatov David <davidsahmatov4@gmail.com>
2023-10-09 12:24:35 +0300

    fixed URl

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { CartProvider } from './context/CartContext';
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/KavaFm'>
     <React.StrictMode>
       <CartProvider>
         <App />
